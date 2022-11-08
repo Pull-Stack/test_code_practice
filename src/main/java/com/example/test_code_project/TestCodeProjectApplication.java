0e08ec8d48b25547dc80749576718e9fb73f3bd0
이재원 <jaewon971111@gmail.com>
2022-11-08 16:04:50 +0900

    initial setting

---
+++ src/main/java/com/example/test_code_project/TestCodeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.test_code_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCodeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestCodeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/test_code_project/TestCodeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.test_code_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestCodeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
